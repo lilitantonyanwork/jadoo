a93ee3e595525b0f828460bfdf18347a5bb395b6
lilitantonyan <lilit@helix.am>
2025-11-21 17:12:07 +0400

    add fixing

--- js/main.js
+++ js/main.js
@@ -18,11 +18,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu');
     const overlay = document.querySelector('.overlay');
     const body = document.querySelector('body');
+    const lang_selected = document.querySelector('.nav__lang--selected');
+    const lang = document.querySelector('.nav__lang--list');
+
     if (menu_btn) {
         menu_btn.addEventListener('click', function (e) {
             menu.classList.toggle('open');
             overlay.classList.toggle('open');
             body.classList.toggle('no-scroll');
+        });
+    }
+    if (lang_selected) {
+        lang_selected.addEventListener('click', function (e) {
+            lang.classList.toggle('open');
+            lang_selected.classList.toggle('open');
         });
     }
 
